37820dae999e94382c1fe23102befc4182cdd49b
cj <745960380@qq.com>
2024-05-22 00:53:38 +0800

    fix: ui

--- src/components/TacToeMode/SelectPlane.tsx
+++ src/components/TacToeMode/SelectPlane.tsx
@@ -52,7 +52,7 @@ const SelectPlane = ({
                     borderBottom: "none",
                     height: isPc
                         ? "calc(100vh - 400px)"
-                        : "calc(100vh - 300px)",
+                        : "calc(100vh - 250px)",
                     marginTop: "10px",
                     overflow: "auto",
                 }}
