51836f5b141b289c3b5ec08aa160266f63e1d6fb
cj <745960380@qq.com>
2024-06-14 00:33:18 +0800

    修改接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL:
+        process.env.NODE_ENV == "production"
+            ? "http://118.139.164.191:3333"
+            : "http://localhost:3333",
 });
 
 export const login = async (data: any) => {
