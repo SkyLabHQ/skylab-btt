724aa5cde3035757efbd1ba2900f71faf8d2834f
cj <745960380@qq.com>
2024-06-19 20:26:46 +0800

    fix: update tg config

--- src/skyConstants/tgConfig.ts
+++ src/skyConstants/tgConfig.ts
@@ -1,4 +1,4 @@
 export const MINI_APP_URL =
     process.env.NODE_ENV == "production"
-        ? "https://t.me/testtesttesttesttest_bot/hahaha"
-        : "https://t.me/test_cj_cj_bot/newapp1";
+        ? "https://t.me/test_cj_cj_bot/newapp1"
+        : "https://t.me/testtesttesttesttest_bot/hahaha";
